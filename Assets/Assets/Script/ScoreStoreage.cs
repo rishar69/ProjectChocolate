81189ac061515e6780d36d9b66ccffd6fa58378d
Ismail4537 <horispararoh457@gmail.com>
2025-02-20 21:43:06 +0700

    HighScore Counter

--- Assets/Assets/Script/ScoreStoreage.cs
+++ Assets/Assets/Script/ScoreStoreage.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 using TMPro;
 public class ScoreStoreage : MonoBehaviour
 {
+    public bool reset;
     public int Score = 0;
+    public int HighScore = 0;
     public TMP_Text ScoreText;
+    public TMP_Text HighScoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ScoreText.text = Score.ToString();
+        if (reset)
+        {
+            PlayerPrefs.SetInt("HighScore", 0);
+        }
+        HighScore = PlayerPrefs.GetInt("HighScore", 0);
+        ScoreText.text = Score.ToString() + " : Score";
+        HighScoreText.text = HighScore.ToString() + " : High Score";
     }
 
     // Update is called once per frame
@@ -18,6 +27,12 @@ public class ScoreStoreage : MonoBehaviour
     public void increaseScore(int value)
     {
         Score += value;
-        ScoreText.text = Score.ToString();
+        ScoreText.text = Score.ToString() + " : Score";
+        if (HighScore < Score)
+        {
+            HighScore = Score;
+            HighScoreText.text = HighScore.ToString() + " : High Score";
+            PlayerPrefs.SetInt("HighScore", HighScore);
+        }
     }
 }
